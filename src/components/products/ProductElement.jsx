03e1b982a04573a70da9e07125cd06a58baf653f
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2021-06-13 09:22:52 -0600

    errors on product element creation - test map function?

---
+++ src/components/products/ProductElement.jsx
@@ -0,0 +1,5 @@
+const ProductCard = (product) => {
+    return ( <h3>test Product</h3> );
+}
+ 
+export default ProductCard;
--- src/components/products/ProductPage.jsx
+++ src/components/products/ProductPage.jsx
@@ -1,9 +1,56 @@
+import React, { useContext, useState }  from 'react';
+import AuthContext from "./../../auth/AuthContext";
+
+import firebaseApp from './../../firebase/firebaseConfig';
+import ProductCard from './ProductElement';
+
 const ProductPage = (props) => {
-    return ( 
-        <>
-        <h1>PRODUCTPAGE</h1>
-        </>
-     );
+    //on page load interface rtdb for DB items object in "products"
+    //populate page with objects
+
+    //if authenticated - load page with edit fields
+    const auth = useContext(AuthContext);
+    var database = firebaseApp.database();
+
+    function CreateProductElement(product){
+        //create a react element based on the product being passed
+    }
+
+    if(auth.authenticate){
+        database.ref().child('products').get().then((snapshot) => {
+            let products = snapshot.val();
+            let productElements = products.map(product => {
+                // let productElement = CreateProductElement(product);
+                let productElement = <ProductCard/>
+                 console.log(productElement);
+                return productElement;
+              });
+            
+            //populate elements
+            
+
+            console.log(productElements);
+            //print to dom
+            return ( 
+                <>
+                <h1>auth PRODUCTPAGE</h1>
+                {/* {productElements} */}
+                </>
+             );
+
+        })
+    }
+    //if !auth - load page without edit fields
+    else{
+        return ( 
+            <>
+            <h1> not auth PRODUCTPAGE</h1>
+            </>
+         );
+    }
+
+
+   
 }
  
 export default ProductPage;
